319d5a9374c1f1d87dc7351c00b13aac888faa53
Zooperman <joy.calcutta@gmail.com>
2018-11-23 21:03:32 +0530

    Changes:Added - Added vscode to gitignore - Re-formatted as per flake8

--- server.py
+++ server.py
@@ -1,48 +1,59 @@
 import os
-from flask import Flask, request
-from flask_restful import Resource, Api
-from sqlalchemy import create_engine
 from json import dumps
-from flask import jsonify
+
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+from sqlalchemy import create_engine
 
 db_connect = create_engine('sqlite:///data/chinook.db', echo=True)
 app = Flask(__name__)
 api = Api(app)
 app.config['DEBUG'] = os.environ.get('DEBUG', False)
 
+
 class Employees(Resource):
     def get(self):
-        conn = db_connect.connect() # connect to database
-        query = conn.execute("select * from employees") # This line performs query and returns json result
-        return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
+        conn = db_connect.connect()  # connect to database
+        # This line performs query and returns json result
+        query = conn.execute("select * from employees")
+        # Fetches first column that is Employee ID
+        return {'employees': [i[0] for i in query.cursor.fetchall()]}
+
 
 class Tracks(Resource):
     def get(self):
         conn = db_connect.connect()
-        query = conn.execute("select trackid, name, composer, unitprice from tracks;")
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        query = conn.execute(
+            "select trackid, name, composer, unitprice from tracks;")
+        result = {'data': [dict(zip(tuple(query.keys()), i))
+                           for i in query.cursor]}
         return jsonify(result)
+
 
 class Employees_Name(Resource):
     def get(self, employee_id):
         conn = db_connect.connect()
-        query = conn.execute("select * from employees where EmployeeId =%d "  %int(employee_id))
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        query = conn.execute(
+            "select * from employees where EmployeeId =%d " % int(employee_id))
+        result = {'data': [dict(zip(tuple(query.keys()), i))
+                           for i in query.cursor]}
         return jsonify(result)
+
 
 class Genres(Resource):
     def get(self):
         conn = db_connect.connect()
         query = conn.execute("select * from genres;")
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        result = {'data': [dict(zip(tuple(query.keys()), i))
+                           for i in query.cursor]}
         return jsonify(result)
 
-api.add_resource(Employees, '/employees') # Route_1
-api.add_resource(Tracks, '/tracks') # Route_2
-api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
-api.add_resource(Genres, '/genres') # Route 4
+
+api.add_resource(Employees, '/employees')  # Route_1
+api.add_resource(Tracks, '/tracks')  # Route_2
+api.add_resource(Employees_Name, '/employees/<employee_id>')  # Route_3
+api.add_resource(Genres, '/genres')  # Route 4
 
 
 if __name__ == '__main__':
-     app.run(port='5002')
-     
+    app.run(port='5002')
